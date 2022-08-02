79849b9b1b0a1c17b64e7e4a8e54ba4c9180b8f9
Savely <Savka9765@yandex.ru>
2022-08-02 20:49:44 +0300

    сделал красиво

--- Hello world/Пример 4/Program.cs
+++ Hello world/Пример 4/Program.cs
@@ -1,4 +1,8 @@
-﻿double NumberA = 100;
-double NumberB = 6;
+﻿double NumberA = new Random().Next(1,1000);
+Console.Write(NumberA);
+Console.Write(" / ");
+double NumberB = new Random().Next(1,100);
+Console.Write(NumberB);
 double result = NumberA/NumberB;
+Console.Write(" = ");
 Console.WriteLine(result);
